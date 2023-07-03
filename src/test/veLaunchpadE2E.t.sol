9a68ec20de87c27dbc3c1b9b2e243ebc642308d1
José Ribeiro <me@joseribeiro.dev>
2023-07-03 15:06:50 -0300

    add fee distributor test

--- src/test/veLaunchpadE2E.t.sol
+++ src/test/veLaunchpadE2E.t.sol
@@ -7,20 +7,26 @@ import "../../utils/VyperDeployer.sol";
 
 import "../IVotingEscrow.sol";
 import "../MyToken.sol";
+import "../FeeDistributor.sol";
 
 abstract contract HelperContract {
     VyperDeployer vyperDeployer = new VyperDeployer();
 
     MyToken token;
+    MyToken weth;
+    FeeDistributor feeDistributor;
 
     IVotingEscrow votingEscrow;
 
     constructor() {
         token = new MyToken("Voting Escrowed Test Token","veTEST");
+        weth = new MyToken("Wrapped ETH","WETH");
 
         votingEscrow = IVotingEscrow(
             vyperDeployer.deployContract("VotingEscrow", abi.encode(token, "Voting Escrowed Test Token", "veTEST"))
         );
+
+        feeDistributor = new FeeDistributor(votingEscrow, 604800);
     }
 }
 
@@ -62,7 +68,7 @@ contract veLaunchPadE2E is Test, HelperContract {
 
         votingEscrow.create_lock(amount, block.timestamp + year);
         require(token.balanceOf(address(this)) == 0);
-        require(votingEscrow.epoch()==1);
+        require(votingEscrow.epoch() == 1);
 
         require(votingEscrow.totalSupply() > 0);
         require(votingEscrow.totalSupply(block.timestamp + year) == 0);
@@ -73,10 +79,79 @@ contract veLaunchPadE2E is Test, HelperContract {
         uint256 year = 365 * 86400;
         token.mint(address(this), amount);
         token.approve(address(votingEscrow), amount);
-        votingEscrow.create_lock(amount/2, block.timestamp + year);
+        votingEscrow.create_lock(amount / 2, block.timestamp + year);
         uint256 amountBefore = votingEscrow.totalSupply();
-        votingEscrow.increase_amount(amount/2);
+        votingEscrow.increase_amount(amount / 2);
         uint256 amountAfter = votingEscrow.totalSupply();
         require(amountAfter > amountBefore);
+    }
+
+    function testFeeDistributor() public {
+        uint256 amount = 10e18;
+        uint256 year = 365 * 86400;
+
+        require(votingEscrow.totalSupply() == 0);
+        require(token.balanceOf(address(this)) == 0);
+        require(token.totalSupply() == 0);
+        require(votingEscrow.epoch() == 0);
+
+        token.mint(address(this), amount);
+        require(token.balanceOf(address(this)) == amount);
+
+        token.approve(address(votingEscrow), 1e18);
+        votingEscrow.create_lock(1e18, block.timestamp + year);
+
+        require(token.balanceOf(address(this)) == 9e18);
+        require(votingEscrow.epoch() == 1);
+
+        require(votingEscrow.totalSupply() > 0);
+        require(votingEscrow.totalSupply(block.timestamp + year) == 0);
+
+        require(feeDistributor.getVotingEscrow() == votingEscrow);
+
+        vm.warp(604801);
+
+        token.approve(address(feeDistributor), 2e18);
+        feeDistributor.depositToken(token, 2e18);
+        require(feeDistributor.getTokenLastBalance(token) == 2e18);
+
+        // require(feeDistributor.getTotalSupplyAtTimestamp(604800) == votingEscrow.totalSupply(604800));
+        require(feeDistributor.getUserTokenTimeCursor(address(this), token) == 604800);
+
+        feeDistributor.checkpoint();
+        feeDistributor.checkpointUser(address(this));
+        feeDistributor.checkpointToken(token);
+
+        vm.warp(604802);
+
+        require(feeDistributor.getUserBalanceAtTimestamp(address(this), 604800) == 978082191757238400);
+        require(feeDistributor.getTotalSupplyAtTimestamp(604800) == 978082191757238400);
+
+        vm.warp(604800 * 2);
+
+        feeDistributor.claimToken(address(this), token);
+        require(token.balanceOf(address(this)) == 9e18);
+    }
+
+    function testE2E() public {
+        // Deploy 2 ERC20 -> BAL, WETH;
+        // Mint 1000 BAL, 1000 WETH;
+        // Deploy Weighted Pool factory;
+        // Deploy 80/20 Pool;
+        // Deploy Voting Escrow;
+        // Deploy FeeDistributor;
+
+        // Mint 100 BAL to A;
+        // User A to join pool with 100 BAL;
+        // User A to lock pool BPT in Voting Escrow;
+
+        // Mint 200 BAL to B;
+        // User B to join pool with 200 BAL;
+        // User B to lock pool BPT in Voting Escrow;
+
+        // Admin deposits 90 BAL in FeeDistributor;
+
+        // User A claims 30 BAL from FeeDistributor;
+        // User B claims 60 BAL from FeeDistributor;
     }
 }
